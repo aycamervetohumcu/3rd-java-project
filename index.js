ece65872686281d0f1770a726ac2a7846d040caf
aycamervetohumcu <amucar06@gmail.com>
2024-10-23 09:25:53 +0200

    console.log is changed

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 let username = prompt("Can you enter your username?");
-
 let password = prompt("Can you enter the password? (must be at least 8 characters, cannot contain spaces, cannot contain the username)");
-
-function isValidPassword(password, username) {
+function isValidPassword(username, password) {
     if (password.length < 8) {
         return false;
     }
-
     if (password.includes(" ")) {
         return false;
     }
-
-    if (password.toLowerCase().includes(username.toLowerCase())) {
+    if (password.includes(username)) {
         return false;
     }
-
     return true;
 }
-
-let result = isValidPassword(password, username);
-
-if (result) {
-    console.log("Password is valid.");
-} else {
-    console.log("Password is not valid.");
-}
+    console.log(isValidPassword(username,password));
